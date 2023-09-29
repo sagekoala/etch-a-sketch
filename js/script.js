50a53637cd7558447ca92bae92e8ce308b203fca
sagekoala <brandondukescodes@gmail.com>
2023-09-29 17:58:05 -0500

    Add functions to control drawing, change drawing mode, and reset box style
    Add eventListener to buttons for click, mouseover, and clear.
    The click button resets the box style and sets the drawing mode to
    click. Similarly, the mouseover button resets the box style and sets
    drawing mode to mouseover. The clear button resets the box style and
    removes all event listeners - i.e. removes drawing mode.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 // Program to create an etch-a-sketch, 16 x 16 grid of square divs using js
 
-// Create reference to body element and create a mainContainer div
-const bodyReference = document.querySelector('body');
-const mainContainer = document.createElement('div');
+// Create reference to mainContainer div
+const gridContainer = document.querySelector('.gridContainer');
 
-let BOXES = 16;
+const BOXES = 16; // Size of grid
 for (let i = 0; i < BOXES; i++) {
     
     // Initialize new inner container
@@ -18,13 +17,82 @@ for (let i = 0; i < BOXES; i++) {
         innerContainer.appendChild(newDiv); // Append all newDiv to innerContainer 
     }
 
-    mainContainer.appendChild(innerContainer); // Append all innerContainer to mainContainer
+    gridContainer.appendChild(innerContainer); // Append all innerContainer to gridContainer
 }
 
-// Append grid to the body element
-bodyReference.appendChild(mainContainer);
+// List of boxes
+const boxList = document.querySelectorAll('.box');
+
+function addClickDrawing() {
+    boxList.forEach((box) => {
+        box.addEventListener('click', clickEventHandler);
+    })
+}
+
+// Click drawing functions
+function clickEventHandler(e) {
+    e.srcElement.classList.add('active');
+}
+
+function removeClickDrawing() {
+    // Remove event listener for clicks
+    boxList.forEach((box) => {
+        box.removeEventListener('click', clickEventHandler);
+    });
+
+    // Reset box style
+    resetBoxStyle();
+}
 
 
+function addMouseoverDrawing() {
+    boxList.forEach((box) => {
+        box.addEventListener('mouseover', mouseoverEventHandler);
+    });
+}
+
+// Mousover drawing functions
+function mouseoverEventHandler(e) {
+    e.srcElement.classList.add('active');
+}
+
+function removeMouseoverDrawing() {
+
+    // Remove mouseover event listener
+    boxList.forEach((box) => {
+        box.removeEventListener('mouseover', mouseoverEventHandler);
+    });
+
+    // Reset box style
+    resetBoxStyle();
+}
+
+// Set up eventListener on clear button
+const clearButton = document.querySelector('.clear');
+clearButton.addEventListener('click', (e) => {
+    removeMouseoverDrawing();
+    removeClickDrawing();
+});
+
+// Set up eventListener on click button
+const clickButton = document.querySelector('.click');
+clickButton.addEventListener('click', () => {
+    removeMouseoverDrawing();
+    addClickDrawing();
+});
+
+// Set up eventListener on mouseover button
+const mouseoverButton = document.querySelector('.mouseover');
+mouseoverButton.addEventListener('click', () => {
+    removeClickDrawing();
+    addMouseoverDrawing();
+});
+
+function resetBoxStyle() {
+    boxList.forEach((box) => {
+        box.classList.remove('active');
+    });
+}
 
 
 
